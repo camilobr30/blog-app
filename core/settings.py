003966217ef98ddd896d6e639ebcfb982a76bbf5
camilobr30 <camilobr30@gmail.com>
2022-07-06 11:56:31 -0500

    configuracion de theme

--- core/settings.py
+++ core/settings.py
@@ -14,8 +14,13 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '*'
+]
 
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 NPM_BIN_PATH = r"C:\Program Files\nodejs\npm.cmd"
 # Application definition
 
