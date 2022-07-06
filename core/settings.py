8631892f1d77eab6d02ca1a007cf789d01e6abca
camilobr30 <camilobr30@gmail.com>
2022-07-06 17:31:20 -0500

    editar perfil

--- core/settings.py
+++ core/settings.py
@@ -21,6 +21,8 @@ ALLOWED_HOSTS = [
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
+    
 NPM_BIN_PATH = r"C:\Program Files\nodejs\npm.cmd"
 # Application definition
 
