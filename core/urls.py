f9fccc0d554c0cdc5dd2787009beda0cd1965a59
camilobr30 <camilobr30@gmail.com>
2022-07-06 13:06:01 -0500

    registro y logueo

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,12 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from .views import HomeView
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
     path('', HomeView.as_view(), name="home"),
 ]
 
